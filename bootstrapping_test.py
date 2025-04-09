610bfb57d996852426442607aed71f36888c0c45
mingi <mintim01@naver.com>
2025-04-09 13:07:56 +0900

    feat: digit remove 테스트 중

--- bootstrapping_test.py
+++ bootstrapping_test.py
@@ -21,7 +21,7 @@ T = 2
 fv_mod_q = fv_scheme.FV_SH(degree=n, pt_modulus=t, ct_modulus=q)
 fv_mod_q.relinearisation_key = fv_mod_q.generate_relinearisation_version1_key(T=2)
 
-def create_ring(q, t, T):
+def create_ring(q, t):
 
     delta = math.floor(q / t)
     print("q = ", q, "t = ", t, "delta = ", delta)
@@ -31,8 +31,8 @@ def create_ring(q, t, T):
     return fv
 
 
-def modulus_switching(ciphertext, q, q_prime):
-    factor = q_prime / q
+def modulus_switching(ciphertext, before_modulus, after_modulus):
+    factor = after_modulus / before_modulus
     def process_component(component):
         if isinstance(component, list):
             return [round(factor * x) for x in component]
@@ -63,40 +63,45 @@ def bootstrapping_with_encrypted_sk(ciphertext, fv_mod_Q, Q, q_prime):
 
     return [e0, e1]
 
-def digit_extraction(z, e, p):
+def digit_extraction(ciphertext, e, p, ring):
     row = e + 1
     col = e + 1
     # 2차원 리스트 초기화
     w = [[0] * col for _ in range(row)]
-    w[0][0] = z
-    Fe = [0, 0, 1]  # X^2
+    w[0][0] = ciphertext
+    
 
     for k in range(e):
-        y = z
+        y = ciphertext
         for j in range(k + 1):
-            w[j][k + 1] = np.polyval(Fe[::-1], w[j][k])
-            y = (y - w[j][k + 1]) / p
+            w[j][k + 1] = ring.multiply_use_rlk_ver1(ct1=w[j][k], ct2=w[j][k], T=2, rlk=ring.relinearisation_key)
+            # w 계수에 음수 처리
+            inverse_w1 = [coef * -1 for coef in w[j][k + 1][0]]
+            inverse_w2 = [coef * -1 for coef in w[j][k + 1][1]]
+            inverse_w = inverse_w1, inverse_w2
+            # y = y - w[j][k + 1] / p
+            y = ring.add(y, inverse_w)
+            y1 = [coef // p for coef in y[0]]
+            y2 = [coef // p for coef in y[1]]
+            y = y1, y2
         w[k + 1][k + 1] = y
-        
     return w
 
-def digit_remove(z, p, e, v):
-    w = digit_extraction(z, e, p)
-    # 맨 위 열 인덱스 출력 (칸 맞춤)
-
-    # col = e + 1
-    # header = ["      "] + [f"{j:>10}" for j in range(col)]
-    # print("[" + " ".join(header) + "]")
-
-    # # 각 행마다 좌측에 행 인덱스 출력
-    # for i, row_vals in enumerate(w):
-    #     print("[" + f"{i:>6}" + " " + " ".join(f"{val:>10}" for val in row_vals) + "]")
-    
-    remove_digit_v = z
+def digit_remove(ciphertext, p, e, v, ring):
+    w = digit_extraction(ciphertext, e, p, ring)
+    # print(w)
+   
+    remove_digit_v = ciphertext
     for i in range(v):
-        remove_digit_v -= w[i][e - 1] * pow(p, i)
+        # temp = w[i][e - 1] * pow(p, i)
+        temp = [coef * pow(p, i) for coef in w[i][e - 1][0]], [coef * pow(p, i) for coef in w[i][e - 1][1]]
+        # 음수 처리
+        temp = [-coef for coef in temp[0]], [-coef for coef in temp[1]]
+        # remove_digit_v -= w[i][e - 1] * pow(p, i)
+        remove_digit_v = ring.add(remove_digit_v, temp)
+        
 
-    return int(remove_digit_v % pow(p, e))
+    return remove_digit_v
 
 def before_bootstrapping(ciphertext):
     
@@ -106,7 +111,7 @@ def before_bootstrapping(ciphertext):
     for _ in range(5):
         enc_add_result = fv_mod_q.add(enc_add_result, ciphertext)
     dec_add_ciphertext = fv_mod_q.decrypt(enc_add_result)
-    print("덧셈 후 덧셈 결과 다항식 복호화 결과: \n", dec_add_ciphertext)
+    print("5회 덧셈 후 덧셈 결과 다항식 복호화 결과: \n", dec_add_ciphertext)
     
     # 4회 곱셈, 5회 부터는 곱셈이 불가능
     enc_mul_result = ciphertext
@@ -114,8 +119,12 @@ def before_bootstrapping(ciphertext):
         enc_mul_result = fv_mod_q.multiply_use_rlk_ver1(ct1=ciphertext, ct2=enc_mul_result, T=T, rlk=fv_mod_q.relinearisation_key)
     
     dec_mul_ciphertext = fv_mod_q.decrypt(enc_mul_result)
-    print("곱셈 후 곱셈 결과 다항식 복호화 결과 : \n", dec_mul_ciphertext)
+    print("4회 곱셈 후 곱셈 결과 다항식 복호화 결과 : \n", dec_mul_ciphertext)
 
+    # 5회 째 곱셈 테스트
+    enc_mul_result = fv_mod_q.multiply_use_rlk_ver1(ct1=ciphertext, ct2=enc_mul_result, T=T, rlk=fv_mod_q.relinearisation_key)
+    dec_mul_ciphertext = fv_mod_q.decrypt(enc_mul_result)
+    print("5회 곱셈 후 곱셈 결과 다항식 복호화 결과 : \n", dec_mul_ciphertext)
 
 # 예제 사용
 if __name__ == "__main__":
@@ -138,7 +147,7 @@ if __name__ == "__main__":
     t = pow(2, 20)
     
     # ring q' 생성
-    fv_mod_q_prime = create_ring(q_prime, t, T)
+    fv_mod_q_prime = create_ring(q_prime, t)
     # 같은 pk 사용
     fv_mod_q_prime.public_key = fv_mod_q.public_key
     # 같은 secret key 사용
@@ -147,28 +156,39 @@ if __name__ == "__main__":
     fv_mod_q_prime.relinearisation_key = fv_mod_q.relinearisation_key
 
     ciphertext_modulus_switched = modulus_switching(ciphertext, q, q_prime)
-
     dec_modulus_switched_ciphertext = fv_mod_q_prime.decrypt(ciphertext_modulus_switched)
     print("modulus switching 후 복호화 결과 : \n", dec_modulus_switched_ciphertext)
 
     print("2. dot product with sk")
     Q = pow(2, 100)
+    t_prime = pow(2, 40)
     # ring Q 생성
-    fv_mod_Q = create_ring(Q, t, T)
+    fv_mod_Q = create_ring(Q, t_prime)
     # 같은 sk 사용
     fv_mod_Q.secret_key = fv_mod_q_prime.secret_key
+    fv_mod_Q.relinearisation_key = fv_mod_Q.generate_relinearisation_version1_key(T=2)
     # dot product with sk
-    dot_product_result_ciphertext = bootstrapping_with_encrypted_sk(ciphertext_modulus_switched, fv_mod_Q, Q, q_prime)
-    dec_e0_e1 = fv_mod_Q.decrypt(dot_product_result_ciphertext)
-    print("dot product with sk 후 복호화 결과 : \n", dec_e0_e1)
+    dot_product_result_ciphertext = bootstrapping_with_encrypted_sk(ciphertext_modulus_switched, fv_mod_Q, Q, t_prime)
+    
 
     print("3. digit extraction")
-    test = 17
-    e = 5
+    e = 40
     p = 2
-    res = digit_remove(test, p, e, 1)
-    print("digit extraction 결과 : ", res)
+    digit_remove_result = digit_remove(ciphertext=dot_product_result_ciphertext, p=p, e=e, v= 20, ring=fv_mod_Q)
+    # print("digit extraction 결과 : \n", digit_remove_result)
+    modulus_switched_ciphertext = [modulus_switching(digit_remove_result[0], Q, q), modulus_switching(digit_remove_result[1], Q, q)]
+    # print("modulus switching 후 : \n", modulus_switched_ciphertext)
+    dec_digit_remove_ciphertext = fv_mod_q.decrypt(modulus_switched_ciphertext)
+    print("digit extraction 후 복호화 결과 : \n", dec_digit_remove_ciphertext)
 
+    mul_result = fv_mod_q.multiply_use_rlk_ver1(ct1=modulus_switched_ciphertext, ct2=modulus_switched_ciphertext, T=T, rlk=fv_mod_q.relinearisation_key)
+    dec_mul_result = fv_mod_q.decrypt(mul_result)
+    print("digit extraction 후 곱셈 결과 : \n", dec_mul_result)
+    
+    
+
+
+    
     
     
    
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,6 @@
- # #1. modulus switching
-    # # 2^60 -> 2^40
-    # q_prime = pow(2, 40)
-    # t = pow(2, 20)
-    # T = 2
-    # # ring q' 생성
-    # fv_mod_q_prime = create_ring(q_prime, t, T)
-
-    # enc_plaintext1_modulus_switched = modulus_switching(enc_plaintext1, q, q_prime)
+    print("3. digit extraction")
+    e = 40
+    p = 2
+    digit_remove_result = digit_remove(ciphertext=modulus_switching_result, p=p, e=e, v=1, ring=fv_mod_Q)
+    print("digit extraction 결과 : \n", digit_remove_result)
+   
